275849bf9dc1f28316e459f81da58e5beb8b9239
ElStevenn <sfukinguay@gmail.com>
2024-12-16 19:56:41 +0100

    come on

---
+++ scripts/CI/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Paths
+PRIVATE_KEY="/home/ubuntu/Main_API_Secret_Project/src/security/private_key.pem"
+PUBLIC_KEY="/home/ubuntu/Main_API_Secret_Project/src/security/public_key.pem"
+
+
+# Creaste public and private key
+if [ ! -f "$PRIVATE_KEY" ]; then
+    echo "Private key not found. Generating private key..."
+    openssl genpkey -algorithm RSA -out "$PRIVATE_KEY" -pkeyopt rsa_keygen_bits:4096
+
+    echo "Extracting public key from the private key..."
+    openssl rsa -pubout -in "$PRIVATE_KEY" -out "$PUBLIC_KEY"
+else
+    echo "Private key already exists. Skipping key generation."
+fi
+# Create .env file
+if [ ! -f "/home/ubuntu/Main_API_Secret_Project/src/.env" ]; then
+    touch /home/ubuntu/Main_API_Secret_Project/src/.env
+fi
+
+
+# Variables
+image_name="fundy_main_api"
+container_nme="fundy_main_api_v1"
+network_name="my_network"
+
+# Stop and remove the application container
+docker container stop "$container_nme"
+docker container rm "$container_nme"
+
+# Build image
+cd /home/ubuntu/Main_API_Secret_Project
+docker build -t "$image_name" .
+
+# Run the application container on the custom network and expose port 80
+docker run -d -p 8080:80 --name "$container_nme" --network "$network_name" "$image_name"
+
+
+
---
+++ scripts/CI/source.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+config="/home/ubuntu/scripts/config.json"
+
+# Variables
+network_name="my_network"
+volumes_name="my_volumes"
+postgres_image="my_postgres"
+postgres="my_postgres_v1"
+
+# Configure
+if [ -f "$config" ]; then
+    echo "Config file found"
+
+    # Check if the file has content
+    if [[ -s "$config" ]]; then
+
+        NETWORK=$(jq -r '.network' "$config")
+        VOLUMES=$(jq -r '.volumes' "$config")
+        POSTGRES=$(jq -r '.postgres' "$config")
+        FIRST_TIME=$(jq -r '.first_time' "$config")
+
+        if [[ "$NETWORK" == "false" ]]; then
+            # Set up network
+            echo "Setting up network"
+            docker network create --driver bridge "$network_name"
+
+            jq '.network = true' "$config" > temp.json && mv temp.json "$config"
+        fi
+
+        if [[ "$VOLUMES" == "false" ]]; then
+            # Set up volumes
+            echo "Setting up volumes"
+            docker volume create --name "$volumes_name"
+
+            jq '.volumes = true' "$config" > temp.json && mv temp.json "$config"
+        fi
+
+        if [[ "$POSTGRES" == "false" ]]; then
+            # Set up postgres
+            echo "Setting up postgres"
+            docker pull postgres:13.2
+            docker run -d \
+                --name "$postgres" \
+                --network "$network_name" \
+                -e POSTGRES_PASSWORD=test_password \
+                -e POSTGRES_USER=test_user \
+                -e POSTGRES_DB=main_db \
+                -v "$volumes_name":/var/lib/postgresql/data \
+                postgres:13.2
+
+            jq '.postgres = true' "$config" > temp.json && mv temp.json "$config"
+        fi
+
+        if [[ "$FIRST_TIME" == "true" ]]; then
+            # Set up first time
+            echo "Running first-time setup..."
+            git clone https://github.com/ElStevenn/Fundy-Main-API.git
+            cd Fundy-Main-API
+
+            jq '.first_time = false' "$config" > temp.json && mv temp.json "$config"
+
+        else
+            git pull origin main
+            cd Fundy-Main-API
+        fi
+
+        # Call Static code analysis -> Build Testing Application -> Run Tests | 
+        bash /home/ubuntu/scripts/CI/build.sh
+
+    else
+        echo "Config file is empty"
+    fi
+
+else
+    echo "Config file not found"
+fi
