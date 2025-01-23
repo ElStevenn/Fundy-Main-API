b01ff462c3534fad2a3fef85fee3f3fb4bcdb104
ElStevenn <sfukinguay@gmail.com>
2025-01-23 12:26:12 +0100

    a

--- src/main.py
+++ src/main.py
@@ -119,7 +119,7 @@ app.include_router(trading_bots)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://fundy.pauservices.top"] if DOMAIN else ["*"],
+    allow_origins=["https://fundy.pauservices.top"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
